11b9e3a6f2935a11a6b6dcccc07e361b98d74f4a
Banciu, Cosmin <cosmin.banciu667@gmail.com>
2023-11-24 14:46:19 +0200

    added tg commands

--- bash-scripts/terragrunt-playground.sh
+++ bash-scripts/terragrunt-playground.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # optimisations
-export TERRAGRUNT_FETCH_DEPENDENCY_OUTPUT_FROM_STATE=true
+export TERRAGRUNT_FETCH_DEPENDENCY_OUTPUT_FROM_STATE=true # can speed up things, supported only with s3 backend, experimental
 export TERRAGRUNT_USE_PARTIAL_PARSE_CONFIG_CACHE=true
 
 terragrunt destroy --auto-approve
@@ -22,3 +22,22 @@ terragrunt plan
 #     private_ipv4_address = subnet_mapping.value
 #   }
 # }
+#
+#
+# Use "-terragrunt-log-level debug" to debug
+#
+#
+terragrunt run-all plan --terragrunt-source-update --terragrunt-non-interactive --auto-approve
+terragrunt run-all apply --terragrunt-source-update --terragrunt-non-interactive
+
+# CAHE
+find . -type d -name ".terragrunt-cache" -prune -exec rm -rf {} \;
+export TERRAGRUNT_DOWNLOAD='/Users/USERNAME/.terragrunt-cache'
+export TF_PLUGIN_CACHE_DIR="$HOME/.terraform.d/plugin-cache"
+
+#DEBUGING
+terragrunt plan render-json
+terragrunt destroy --auto-approve -terragrunt-log-level debug
+
+# PARALLELISM
+terragrunt run-all apply --terragrunt-parallelism 4 # for "text file busy" error
