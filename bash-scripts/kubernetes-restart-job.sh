93019c8abb9d4fc71b5cd1fcf2838ed9740d643e
Banciu, Cosmin <cosmin.banciu667@gmail.com>
2024-05-09 18:17:39 +0300

    minor changes and reorg

--- bash-scripts/kubernetes-restart-job.sh
+++ bash-scripts/kubernetes-restart-job.sh
@@ -8,10 +8,21 @@ fi
 job_name="$1"
 namespace="$2"
 
-kubectl get job ${job_name} -n ${namespace} -o yaml | yq eval 'del(.spec.selector)' - | yq eval 'del(.spec.template.metadata.labels."controller-uid")' - | yq eval 'del(.metadata.creationTimestamp)' - | yq eval 'del(.metadata.selfLink)' - | yq eval 'del(.metadata.uid)' - | yq eval 'del(.metadata.resourceVersion)' - | yq eval 'del(.metadata.managedFields)' - | yq eval 'del(.status)' - > job.yaml
+kubectl get job ${job_name} -n ${namespace} -o yaml | \
+  yq eval 'del(.spec.selector)' - | \
+  yq eval 'del(.spec.template.metadata.labels."controller-uid")' - | \
+  yq eval 'del(.spec.template.metadata.labels."batch.kubernetes.io/controller-uid")' - | \
+  yq eval 'del(.spec.template.metadata.labels."batch.kubernetes.io/job-name")' - | \
+  yq eval 'del(.spec.template.metadata.labels."job-name")' - | \
+  yq eval 'del(.metadata.creationTimestamp)' - | \
+  yq eval 'del(.metadata.selfLink)' - | \
+  yq eval 'del(.metadata.uid)' - | \
+  yq eval 'del(.metadata.resourceVersion)' - | \
+  yq eval 'del(.metadata.managedFields)' - | \
+  yq eval 'del(.status)' - > job.yaml
 
 kubectl delete job ${job_name} -n ${namespace}
 kubectl create -f job.yaml -n ${namespace}
 
 # Optional: Remove the job.yaml file after recreation
-rm job.yaml
+# rm job.yaml
